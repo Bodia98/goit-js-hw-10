1412b13df3ebd570bd8dc80373fdeaeaa464f488
Bodia98 <stefanyuk1998@gmail.com>
2023-02-23 12:48:44 +0200

    fixed

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,15 +1,15 @@
 export default class ApiCountries {
-    constructor() {
+  constructor() {
     this.searchQuery = '';
-    }
+  }
 
-    fetchContry() {
+  fetchContry() {
     const url = `https://restcountries.com/v3.1/name/${this.searchQuery}?fields=name,capital,population,flags,languages`;
     return fetch(url).then(r => {
-        if (!r.ok) {
-            throw new Error(r.status);
-        }
-            return r.json();
+      if (!r.ok) {
+        throw new Error(r.status);
+      }
+      return r.json();
     });
-    }
+  }
 }
